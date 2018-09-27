473a355a920f31768e1ed23b103a809afa099109
unknown <diegosimoncarmona@gmail.com>
2018-09-26 21:14:49 -0300

    Commit inicial de criação do projeto

---
+++ src/main/java/com/alga/carmona/api/AlgaApiApplication.java
@@ -0,0 +1,12 @@
+package com.alga.carmona.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaApiApplication.class, args);
+	}
+}
